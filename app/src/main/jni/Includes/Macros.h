3caf7a285621447303cd65078a6ecfc4b9dcef0d
GuoYiFan <2625053283@qq.com>
2026-03-10 10:24:40 +0800

    fix: include MemoryPatch and STL headers

--- app/src/main/jni/Includes/Macros.h
+++ app/src/main/jni/Includes/Macros.h
@@ -2,6 +2,14 @@
 #ifndef ANDROID_MOD_MENU_MACROS_H
 #define ANDROID_MOD_MENU_MACROS_H
 
+#include <algorithm>
+#include <cstdint>
+#include <iterator>
+#include <string>
+#include <vector>
+
+#include "KittyMemory/MemoryPatch.h"
+
 #if defined(__aarch64__) //Compile for arm64 lib only
 #include <And64InlineHook/And64InlineHook.hpp>
 #include "Dobby/dobby.h"
